3748bf5462e8930b628ef4c6e24e7110a3862f4f
z242jian <z242jian@gmail.com>
2024-10-25 20:28:08 -0400

    This is the first commit

---
+++ src/main/java/ut/ece1779/carpool_web_backend/CarpoolWebBackendApplication.java
@@ -0,0 +1,13 @@
+package ut.ece1779.carpool_web_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolWebBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ut/ece1779/carpool_web_backend/CarpoolWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.ece1779.carpool_web_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
